866f1fc6096cd12e2abe26d61d44966636cf57b1
Max Golovsky <max@excursionist.com>
2020-11-10 23:42:15 -0500

    Added sweeping functions

--- DukeModel.py
+++ DukeModel.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
-T_end = 100
+T_end = 100 # In Days
 
 # ON CAMPUS VARIABLES
 Susceptible_c = np.zeros(T_end)
@@ -34,8 +34,6 @@ totSusceptible = np.zeros(T_end)
 totUndetected = np.zeros(T_end)
 
 
-init_uninfect = 4900
-
 death = 0.00004
 recovery_rate = (1 / 14)
 symptom_onset = 0.06122
@@ -45,19 +43,17 @@ specificity = .98
 
 
 # SWEEP VARIABLES
-R_stu = 1
+R_stu_range = np.array([0,1,2])
 R_c = 1
 
-Beta_stu = (recovery_rate + symptom_onset)*R_stu  # Base Case   Each student to each student
-Beta_c = (recovery_rate + symptom_onset)*R_c  # Base Case  Between on campus students
 
-screening = 2/7  # Base Case
+screening_range = np.array([1,2,3])/7  # days per week
 
-X = 1 # Base Case
-Y = 2
+X_range = np.array([0,1,2]) #infections per week
+Y_range = np.array([0,2,4])
 
 
-def plot(U, A, S, R, D, Fp, Tp, t, name):
+def plot(Susceptible, Undetected, Symptomatic, Recovered, Dead, Fp, Tp, t, name, dir, sweptVar):
     fig = plt.figure(num=1, clear=True)
     ax = fig.add_subplot(1, 1, 1)
     # Plot using red circles
@@ -72,28 +68,32 @@ def plot(U, A, S, R, D, Fp, Tp, t, name):
     ax.plot(t, Tp, 'y-', label='Isolated (Asymptomatic)')
     ax.plot(t, D, 'k-', label='Dead')
     """
-    totalInfected = np.array([S, Tp])
-    ax.plot(t, A, 'g-', label='Infected (Undetected)')
-    ax.plot(t, totalInfected.sum(axis=0), 'r-', label='Infected (Isolated)')
+
+    totalInfectedIsolated = np.array([Symptomatic, Tp])
+    ax.plot(t, Undetected, 'g-', label='Infected (Undetected)')
+    ax.plot(t, totalInfectedIsolated.sum(axis=0), 'r-', label='Infected (Isolated)')
     # Set labels and turn grid on
     ax.set(xlabel='Time, Days', ylabel=r'Population', title=name)
     ax.grid(True)
     ax.legend(loc='upper right')
     # Use space most effectively
     fig.tight_layout()
-    fig.savefig("{}.png".format(name.replace(' ', '_')))
+    fig.savefig("{}/{}_{}.png".format(dir, name.replace(' ', '_'), sweptVar))
     fig.show()
 
 
-def OnCampus(t):
+def OnCampus(t, R_stu, screening, X):
     infectedFrac = (
             totUndetected[t] / (totSusceptible[t] + totUndetected[t]))  # Fraction of non-isolated students infected
 
     infectedFrac_c = Undetected_c[t] / (Susceptible_c[t] + Undetected_c[t])
 
+    Beta_stu = (recovery_rate + symptom_onset) * R_stu  # Each student to each student
+    Beta_c = (recovery_rate + symptom_onset) * R_c  # Between on campus students
+
     if t%7 ==0:
         x = X
-        print("New Infections On Campus: {:.3}".format(Iso_in_c[t]+Sympt_c[t]))
+        #print("New Infections On Campus: {:.3}".format(Iso_in_c[t]+Sympt_c[t]))
     else:
         x = 0
 
@@ -122,15 +122,19 @@ def OnCampus(t):
     # print("True Pos:{}, Asympt: {}, Sympt: {}".format(Iso_in_c[t], Undetected_c[t], Sympt_c[t]))
 
 
-def OffCampus(t):
+def OffCampus(t, R_stu, screening, Y):
+
     infectedFrac = (
             totUndetected[t] / (totSusceptible[t] + totUndetected[t]))  # Fraction of non-isolated students infected
 
+    Beta_stu = (recovery_rate + symptom_onset) * R_stu  # Each student to each student
+
     if t%7 ==0:
-        print("New Infections Off Campus: {}".format(Iso_in_o[t]+Sympt_o[t]))
+        #print("New Infections Off Campus: {}".format(Iso_in_o[t]+Sympt_o[t]))
         y = Y
     else:
         y = 0
+
     Susceptible_o[t + 1] = Susceptible_o[t] * (
             1 - Beta_stu * infectedFrac) - Susceptible_o[t - 1] * screening * (1 - specificity) + \
                            mu * Iso_un_o[t] - y
@@ -153,29 +157,51 @@ def OffCampus(t):
     # print("True Pos:{}, Asympt: {}, Sympt: {}".format(Iso_in_o[t], Undetected_o[t], Sympt_o[t]))
 
 
-time = range(0, T_end)
+def Model(R_stu, screening, X, Y, dir, sweptVar):
 
-for t in time[0:T_end - 1]:
-    totSusceptible[t] = Susceptible_c[t] + Susceptible_o[t]
-    totUndetected[t] = Undetected_c[t] + Undetected_o[t]
-    OnCampus(t)
-    OffCampus(t)
-    #if t%7 ==0:
-        #print("Recovered on {:.3}, Recovered off {:.3}".format(Recovered_c[t],Recovered_o[t]))
+    time = range(0, T_end)
 
-    #print("Susceptible: {}\n Undetected: {}".format(totSusceptible[t], totUndetected[t]))
+    for t in time[0:T_end - 1]:
 
-print("Total Recovered {}".format(Recovered_c[99]+Recovered_o[99]))
+        totSusceptible[t] = Susceptible_c[t] + Susceptible_o[t]
+        totUndetected[t] = Undetected_c[t] + Undetected_o[t]
+        OnCampus(t, R_stu, screening, X)
+        OffCampus(t, R_stu, screening, Y)
 
-plot(Susceptible_c, Undetected_c, Sympt_c, Recovered_c, Deaths_c, Iso_un_c, Iso_in_c, time, "On Campus")
-plot(Susceptible_o, Undetected_o, Sympt_o, Recovered_o, Deaths_o, Iso_un_o, Iso_in_o, time, "Off Campus")
-plot(np.add(Susceptible_c,Susceptible_o),
-     np.add(Undetected_c,Undetected_o),
-     np.add(Sympt_c, Sympt_o),
-     np.add(Recovered_c,Recovered_o),
-     np.add(Deaths_c,Deaths_o),
-     np.add(Iso_un_c,Iso_un_o ),
-     np.add(Iso_in_c, Iso_in_o),
-     time, "Overall Data")
 
-# out = odeint(InfectionModel, Yo, t)
+
+    print("Total Recovered {}\n R:{}, Screening:{}, X:{}, Y:{},".format(
+        Recovered_c[99]+Recovered_o[99], R_stu, screening, X, Y))
+
+    campusData = (Susceptible_c, Undetected_c, Sympt_c, Recovered_c, Deaths_c, Iso_un_c, Iso_in_c)
+    offCampusData =(Susceptible_o, Undetected_o, Sympt_o, Recovered_o, Deaths_o, Iso_un_o, Iso_in_o)
+
+    plot(Susceptible_c, Undetected_c, Sympt_c, Recovered_c, Deaths_c, Iso_un_c, Iso_in_c, time, "On Campus",
+         dir, sweptVar)
+    plot(Susceptible_o, Undetected_o, Sympt_o, Recovered_o, Deaths_o, Iso_un_o, Iso_in_o, time, "Off Campus",
+         dir, sweptVar)
+    plot(np.add(Susceptible_c,Susceptible_o),
+         np.add(Undetected_c,Undetected_o),
+         np.add(Sympt_c, Sympt_o),
+         np.add(Recovered_c,Recovered_o),
+         np.add(Deaths_c,Deaths_o),
+         np.add(Iso_un_c,Iso_un_o ),
+         np.add(Iso_in_c, Iso_in_o),
+         time, "Overall Data", dir, sweptVar)
+
+
+sweepVar = int(input("Select Sweep Variable \n 1) Inter-Student R \n 2) Screening Frequency \n 3) Exogenous Shocks \n"))
+
+if sweepVar == 1:
+    for r in R_stu_range:
+        Model(r, screening_range[1], X_range[1], Y_range[1], "R_Stu", "r_{}".format(r))
+
+elif sweepVar == 2:
+    for screen in screening_range:
+        print(screen*7)
+        Model(R_stu_range[1], screen, X_range[1], Y_range[1], "Screening", "Screening_{}".format(screen*7))
+else:
+    for shock in range(0,3):
+        Model(R_stu_range[1], screening_range[1], X_range[shock], Y_range[shock], "Exogenous", "ShockLevel_{}".format(shock))
+
+
